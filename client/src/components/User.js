0affcfb3a3fd9b4166d14b90e2a8622b111d1dec
kongbai23330 <109857471+kongbai23330@users.noreply.github.com>
2023-03-05 10:32:36 +0200

    Add files via upload

---
+++ client/src/components/User.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+function UserProfile() {
+  const [user, setUser] = useState({});
+  const  userId  = localStorage.getItem('userId');
+    const imgurl = localStorage.getItem('profilePicture')
+    // Effect hook to get user data
+  useEffect(() => {
+    const getUserById = () => {
+      fetch(`http://localhost:1234/api/users/${userId}`)
+        .then((response) => response.json())
+        .then((data) => setUser(data))
+        .catch((error) => console.error(error));
+    }
+    
+    getUserById();
+  }, [userId]);
+
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>Email: {user.email}</p>
+      <p>Bio: {user.bio || 'No bio available'}</p>
+      <p>Registered: {new Date(user.registered).toLocaleDateString()}</p>
+      <img src={imgurl} alt="User profile" />
+      </div>
+  );
+}
+
+export default UserProfile;
---
+++ client/src/components/register.js
@@ -0,0 +1,90 @@
+// Importing necessary dependencies and components from MUI and React libraries
+import { useState } from 'react';
+import { Link ,useNavigate} from 'react-router-dom';
+import { AppBar, Toolbar, Button, TextField, Box, Container } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const API_URL = 'http://localhost:1234';
+// Styling the text field component
+const MyTextField = styled(TextField)({
+  marginBottom: '16px',
+});
+// Defining the Register Page component
+const RegisterPage = () => {
+    const navigate = useNavigate();
+    // Defining the state variables for email, password and profile picture
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+    const [profilePicture, setProfilePicture] = useState(null);
+    // Handling the form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+      console.log(password);
+      const image = localStorage.getItem('profilePicture')
+      // Sending a POST request to the server to register the user
+    const response = await fetch(`${API_URL}/api/user/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+        body: JSON.stringify({
+            email,
+            password,
+            // profilePicture:image,
+        })
+    });
+      console.log(email)
+      console.log(password);
+      const data = await response.json();
+      
+// Navigating to the home page after successful registration
+      navigate('/')
+    console.log(data); // TODO: Handle response from server
+    };
+    // Handling the profile picture upload
+  const handlePictureUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      const imageBase64 = reader.result;
+        localStorage.setItem('profilePicture', imageBase64);
+        console.log(localStorage)
+    };
+  };
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit" component={Link} to="/comment">
+            {'My Blog'}
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="xs">
+        <Box sx={{ marginTop: '32px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <h2>Register</h2>
+          <form onSubmit={handleSubmit} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <MyTextField label="Email" variant="outlined" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required sx={{ width: '100%' }} />
+            <MyTextField
+              label="Password"
+              variant="outlined"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              sx={{ width: '100%' }}
+                      />
+                      <label htmlFor="profilePicture">Profile Picture:</label>
+            <input type="file" id="profilePicture" name="profilePicture" onChange={handlePictureUpload} />
+            <Button variant="contained" type="submit">
+              Register
+            </Button>
+          </form>
+        </Box>
+      </Container>
+    </>
+  );
+};
+// Exporting the RegisterPage component
+export default RegisterPage;
